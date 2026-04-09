09b9ef2e9f0a041a0227d0cd0a71bb9bb8a326ce
Casper S Jensen <caspersjensen@outlook.dk>
2026-04-09 10:54:03 +0200

    Changed Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,14 @@ import ContactMe from "./pages/ContactMe";
 
 function App() {
   return (
-    // Set basename to your GitHub repo name
     <BrowserRouter basename="/portfoliowebsitecasper/">
       <Routes>
         {/* MAIN LAYOUT */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-        </Route>
-
-        {/* GitHub LAYOUT */}
-        <Route path="/github" element={<MainLayout />}>
-          <Route index element={<GithubRepoList />} />
-        </Route>
-
-        {/* COMMIT LOG LAYOUT */}
-        <Route path="/github/:id" element={<MainLayout />}>
-          <Route index element={<GithubCommitLog />} />  
-        </Route>
-
-        {/* CONTACT LAYOUT */}
-        <Route path="/contact" element={<MainLayout />}>
-          <Route index element={<ContactMe />} />
+          <Route path="github" element={<GithubRepoList />} />
+          <Route path="github/:id" element={<GithubCommitLog />} />
+          <Route path="contact" element={<ContactMe />} />
         </Route>
       </Routes>
     </BrowserRouter>
