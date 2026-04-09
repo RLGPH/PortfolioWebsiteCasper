c1903c42f024b47a42b1d19afc68cb8915146638
Casper S Jensen <caspersjensen@outlook.dk>
2026-04-09 12:35:46 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PortfolioWebsiteCasper/',
+  base: '/PortfolioWebsiteCasper/', // match your repo name
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-  },
 });
